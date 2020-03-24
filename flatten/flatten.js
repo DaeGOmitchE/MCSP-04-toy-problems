be2b73a81eb3126f16188c6fe50d347924caea3e
elise mitchell <elisemitchell@eyeOsauron.local>
2020-03-24 09:51:22 -0500

    emryFlat

--- flatten/flatten.js
+++ flatten/flatten.js
@@ -2,5 +2,17 @@
 //Your function should return a single array with non-array elements
 // Example:
 // flatten([1,['a', 2, [8, true], 7]]) returns [1, 'a', 2, 8, true, 7]
+//What? Return an array with no subarrays with flatten function
+//How?
+//1.Your function returns Array with no subarrays
+//2.first turn the current array into a string
+//3.turn the newly created string into an array
 
-function flatten(array) {}
+function flatten(array) {
+
+    //turn array into a string
+    return array.join().split;
+
+    //turn string into a new array
+
+}
