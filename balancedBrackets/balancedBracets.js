dccd445a4b151fd98f4e2abee21923b1cc45c5bc
elise mitchell <elisemitchell@eyeOsauron.lan>
2020-04-30 09:56:25 -0500

    balanceEm

--- balancedBrackets/balancedBracets.js
+++ balancedBrackets/balancedBracets.js
@@ -1,5 +1,9 @@
 // Given a string, return true if it contains all balanced parenthesis (),
 // curly-brackets {}, and square-brackets [].
+//I: a string
+//O: boolean
+//C:
+//E:
 
 //                        Examples
 //               Input	                       Output
@@ -9,4 +13,15 @@
 // str: "[{()}]"                  |   true
 // str: "(50)("                   |   false
 // str: "[{]}"                    |   false
-const isBalanced = str => {};
+const isBalanced = str => {
+    // how to rep all balanced bracket pairs with a variable?
+    //loop over str
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] === )
+
+    }
+    //does index of string have ( and ) or { and } or [ and ]?
+    // if yes return true
+    //otherwise
+    //return false
+}        
