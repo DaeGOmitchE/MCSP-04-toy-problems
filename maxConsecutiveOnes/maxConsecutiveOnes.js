6bcb8bd942695d608531d584e970bdfdae603e8c
DaeGOmitchE <57451928+DaeGOmitchE@users.noreply.github.com>
2020-05-01 09:56:58 -0500

    Ones

--- maxConsecutiveOnes/maxConsecutiveOnes.js
+++ maxConsecutiveOnes/maxConsecutiveOnes.js
@@ -1,7 +1,22 @@
 // Write a function that takes in an array of numbers and returns the maximum number of consecutive 1s
 
 //maxConsecutiveOnes([1,1,2,0,3,1,1,1,1,5,6,3]) returns 4
-
+// I: takes in an array of numbers
+// 0: return the mav numdrt of concecutive 1s.
+// C:
+// E:
 function maxConsecutiveOnes(array) {
-  //write solution code
+  // identify a variable for count of ones
+  var count = 0;
+  //iterate over the array
+  for (var i = 0; i < Array.length; i++){
+    // if the number is equal to 1
+  if (array[i] === 1) {
+    
+  }
+  }
+  if
+  // and is consecutive??
+//return count
+
 }
