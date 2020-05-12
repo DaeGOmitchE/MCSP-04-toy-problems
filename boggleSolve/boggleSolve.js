344a107cabc34f81fd87556149962e37d3e0eaee
DaeGOmitchE <57451928+DaeGOmitchE@users.noreply.github.com>
2020-05-12 10:48:49 -0400

    emryalpha

--- boggleSolve/boggleSolve.js
+++ boggleSolve/boggleSolve.js
@@ -2,5 +2,16 @@
 // * each character has the value of its position in the alphabet
 // * i.e. a=1, z=26 etc.
 // return the sum of the values of each letter in string
+// I: string containing a combo of letters
+// O: the sum of the values of each letter
+// C:
+// E:
 
-const boggleSolve = str => {};
+//input: ("b, c, a") output: 6 //a=1, b=2, c=3
+
+const boggleSolve = str => {
+    //declare a total variable
+    //the letter is equal to its position in the alphabet
+    //iterate over the string 
+    //if there is a letter at index
+};
