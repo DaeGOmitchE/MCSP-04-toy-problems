b3c90b6d80bdfcb09716e5ce50222b00cf4cfa3b
DaeGOmitchE <57451928+DaeGOmitchE@users.noreply.github.com>
2020-05-19 10:57:10 -0400

    deletNEM

---
+++ deleteNth/deleteNth.js
@@ -0,0 +1,32 @@
+
+// Given a list lst and a number N, create a new list that contains each number 
+// of lst at most N times without reordering. For example if N = 2, and the input 
+// is [1,2,3,1,2,1,2,3], you take [1,2,3,1,2], drop the next [1,2] since this would
+//  lead to 1 and 2 being in the result 3 times, and then take 3,
+//   which leads to [1,2,3,1,2,3]
+
+//I: given an array and a number
+//O: an array of numbers and n for max number of times
+//C: 
+//E;
+
+// ([4,7,4,12,14,12], 1) since he wants to see each item in the array 
+// no more than one time return the array [4,7,12,14]
+
+// ([1,1,3,3,9,5,5,4,1,5], 3) since he wants to see each item in the array 
+// no more than three times return[1,1,1,3,3,9,5,5,5]
+
+// Example
+//   deleteNth ([1,1,1,1],2) // return [1,1]
+//   deleteNth ([20,37,20,21],1) // return [20,37,21]
+
+
+function deleteNth(arr,n) {
+    //make a result array
+
+    //iterate over the array
+
+    //if item appears less than n times 
+
+    //push item to result array
+}
