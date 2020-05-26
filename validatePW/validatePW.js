377df5dcbbe243ed005b2c792a822dd2bfec88c0
DaeGOmitchE <57451928+DaeGOmitchE@users.noreply.github.com>
2020-05-26 10:49:38 -0400

    PWE

---
+++ validatePW/validatePW.js
@@ -0,0 +1,30 @@
+// I will give you a string. You respond with "VALID" if the string meets the requirements
+//  or "INVALID" if it does not.
+
+// Passwords must abide by the following requirements:
+
+// More than 3 characters but less than 20.
+
+// Must contain only alphanumeric characters.
+
+// Must contain letters and numbers.
+
+//I a string 
+//O if the string is a valid or invalid password
+//C only contain alphanumeric characters
+//E
+
+
+function validPass(password) {
+    //contains at least 3 char
+
+    //contains less than 20 char
+
+    //contains numbers
+
+    // contains uppercase letters
+
+    // contains lowercase letters
+
+
+}
