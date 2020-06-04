2e06c5572026c715caeba7e02cd264c72678fca1
DaeGOmitchE <57451928+DaeGOmitchE@users.noreply.github.com>
2020-06-04 10:47:10 -0400

    em

---
+++ highestProduct/highestProduct.js
@@ -0,0 +1,24 @@
+// Create a function that takes in an integer array and 
+// returns the highest product of three numbers
+
+//Example Inputs:
+// -1 0 2 5 3 -4
+// 10 10 10 10
+// -5 9 6 0
+// 2 -2 4 0 -1 3
+
+//Outputs:
+// 30
+// 1000
+// 0
+// 24
+
+function highestProduct(array) {
+   //make result variable
+   var result = 0;
+   //iterate over the array to get the last number
+   //iterate over the array to get the next to last number
+   //iterate over the array to get the third number
+   //multiply all three numbers
+
+  }
